0fd44c718d13d9cfa9a57d4121b57ded2b0dc242
martin <brissetmartin@gmail.com>
2020-04-24 16:31:20 -0300

    primer commit

---
+++ android/app/src/main/kotlin/com/example/probando_componentes/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.probando_componentes
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
