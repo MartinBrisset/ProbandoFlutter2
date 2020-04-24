aef12dfa5a2394f638a575c741a3234379e88183
martin <brissetmartin@gmail.com>
2020-04-24 17:14:02 -0300

    agregando home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:probando_componentes/src/pages/home_temp.dart';
+import 'package:probando_componentes/src/pages/home_page.dart';
+// import 'package:probando_componentes/src/pages/home_temp.dart';
  
 void main() => runApp(MyApp());
  
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Componentes App',
-      home: HomePageTemp()
+      home: HomePage()
     );
   }
 }
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes'),
+      ),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista() {
+
+    return ListView();
+
+    
+  }
+}
